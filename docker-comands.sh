2e149f5719fa2f1ec99d8c47f06f53d065a54b1c
juniomenchik <juniomenchik@live.com>
2024-05-15 12:44:18 -0300

    Update Docker Ports and Integrate Swagger API Documentation

---
+++ docker-comands.sh
@@ -0,0 +1,8 @@
+ docker container rm spring-boot
+ mvn clean package -DskipTests
+ docker build -t spring-boot:0.0.1 .
+ docker tag spring-boot:0.0.1 juniomenchik/spring-boot:0.0.1
+ docker images
+ docker push juniomenchik/spring-boot:0.0.1
+ docker run -p 8765:8765 juniomenchik/spring-boot:0.0.1
+ docker run --name spring-boot -d -p 8765:8765 juniomenchik/spring-boot:0.0.1
