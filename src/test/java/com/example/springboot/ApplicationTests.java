abaf5b20e637e1b955c125ff849bcf30ea64b2c0
juniomenchik <juniomenchik@live.com>
2024-05-17 15:25:18 -0300

    Remove incorrect assertion in ApplicationTests

--- src/test/java/com/example/springboot/ApplicationTests.java
+++ src/test/java/com/example/springboot/ApplicationTests.java
@@ -1,6 +1,5 @@
 package com.example.springboot;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,7 +8,6 @@ public class ApplicationTests {
 
 	@Test
     public void contextLoads() {
-		Assert.assertEquals(10,20);
 	}
 
 }
