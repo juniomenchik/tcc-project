90c59ebeba72e732e4189720772ebf9ddcdbf91e
juniomenchik <juniomenchik@live.com>
2024-05-17 14:49:45 -0300

    Added assertion in ApplicationTests

--- src/test/java/com/example/springboot/ApplicationTests.java
+++ src/test/java/com/example/springboot/ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.springboot;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ public class ApplicationTests {
 
 	@Test
     public void contextLoads() {
+		Assert.assertEquals(10,20);
 	}
 
 }
